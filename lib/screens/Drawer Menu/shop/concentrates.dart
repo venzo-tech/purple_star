2ef12981fa21979ca7c3b125ed99fdb690fafe8e
elam126 <elam_126@ymail.com>
2021-12-29 00:11:29 +0530

    add to basket

--- lib/screens/Drawer Menu/shop/concentrates.dart
+++ lib/screens/Drawer Menu/shop/concentrates.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 import 'package:purple_star/screens/Model/product_model.dart';
 import 'package:purple_star/screens/Services/product_services.dart';
+import 'package:purple_star/screens/cart/add_cart_page.dart';
 
 class Concentrates extends StatefulWidget {
   const Concentrates({Key? key}) : super(key: key);
@@ -198,7 +199,7 @@ class _ConcentratesState extends State<Concentrates> {
                                           const SizedBox(height: 10),
                                           InkWell(
                                             onTap: () {
-                                              AddToBasket();
+                                              AddToBasket(context,snapshot.data![index]);
                                             },
                                             child: Container(
                                               width: 125,
@@ -244,65 +245,90 @@ class _ConcentratesState extends State<Concentrates> {
       ),
     );
   }
+
+  // Future<void> AddToBasket(Product product) async {
+  //   showModalBottomSheet<void>(
+  //     backgroundColor: Colors.transparent,
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return Padding(
+  //         padding: const EdgeInsets.all(20.0),
+  //         child: Container(
+  //           height: MediaQuery.of(context).size.height * .80,
+  //           decoration: const BoxDecoration(
+  //               color: Colors.white,
+  //               borderRadius: BorderRadius.only(
+  //                   topLeft: Radius.circular(15.0),
+  //                   topRight: Radius.circular(15.0))),
+  //           child: Padding(
+  //             padding: const EdgeInsets.fromLTRB(30, 20, 30, 10),
+  //             child: Column(
+  //               crossAxisAlignment: CrossAxisAlignment.start,
+  //               children: <Widget>[
+  //                 const Text(
+  //                   'added to basket',
+  //                   style: TextStyle(fontFamily: 'BebasNeue', fontSize: 23),
+  //                 ),
+  //                 Row(
+  //                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //                   children: <Widget>[
+  //                     ClipRRect(
+  //                       borderRadius: BorderRadius.circular(
+  //                           0.0), //or 15.0
+  //                       child: SizedBox(
+  //                         height: 100.0,
+  //                         width: 100.0,
+  //                         child: Image(
+  //                           image: NetworkImage(product.productImageUrl),
+  //                         ),
+  //                       ),
+  //                     ),
+  //                     Column(
+  //                       crossAxisAlignment: CrossAxisAlignment.start,
+  //                       children: [
+  //                         Text(product.title),
+  //                         Text('-'+product.made),
+  //                       ],
+  //                     ),
+  //                   ],
+  //                 ),
+  //                 const SizedBox(
+  //                   height: 20,
+  //                 ),
+  //                 SizedBox(
+  //                   width: double.infinity,
+  //                   child: ElevatedButton(
+  //                     style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+  //                     child: const Text('Proceed to checkout',
+  //                         style:
+  //                         TextStyle(fontFamily: 'BebasNeue', fontSize: 17)),
+  //                     onPressed: () => Navigator.pop(context),
+  //                   ),
+  //                 ),
+  //                 SizedBox(
+  //                   width: double.infinity,
+  //                   child: ElevatedButton(
+  //                     style: ElevatedButton.styleFrom(
+  //                       primary: Colors.white,
+  //                       onPrimary: const Color(0xFF562c8a),
+  //                       side: const BorderSide(
+  //                           width: 2.0, color: Color(0xFF562c8a)),
+  //                       shape: const StadiumBorder(),
+  //                     ),
+  //                     child: const Text('Continue shopping',
+  //                         style:
+  //                         TextStyle(fontFamily: 'BebasNeue', fontSize: 17)),
+  //                     onPressed: () => Navigator.pop(context),
+  //                   ),
+  //                 )
+  //               ],
+  //             ),
+  //           ),
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
 }
 
- AddToBasket() {
-  showModalBottomSheet(
-    backgroundColor: Colors.transparent,
-    context: context,
-    builder: (BuildContext context) {
-      return Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Container(
-          height: MediaQuery.of(context).size.height * .80,
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15.0),
-                  topRight: Radius.circular(15.0))),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30, 20, 30, 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Text(
-                  'added to basket',
-                  style: TextStyle(fontFamily: 'BebasNeue', fontSize: 23),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(shape: StadiumBorder()),
-                    child: const Text('Proceed to checkout',
-                        style:
-                            TextStyle(fontFamily: 'BebasNeue', fontSize: 17)),
-                    onPressed: () => Navigator.pop(context),
-                  ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.white,
-                      onPrimary: const Color(0xFF562c8a),
-                      side: const BorderSide(
-                          width: 2.0, color: Color(0xFF562c8a)),
-                      shape: const StadiumBorder(),
-                    ),
-                    child: const Text('Continue shopping',
-                        style:
-                            TextStyle(fontFamily: 'BebasNeue', fontSize: 17)),
-                    onPressed: () => Navigator.pop(context),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      );
-    },
-  );
-}
+
